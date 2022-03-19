428fa4074286be784a9b80676f78da7ed1c33479
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-03-20 00:43:41 +0200

    style open nav menu

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,10 @@ const nav = document.getElementById('nav');
 
 openNavBtn.addEventListener('click', function() {
     nav.classList.add('navigation-open');
+    document.body.classList.add('stop-scrolling');
 });
 
 closeNavBtn.addEventListener('click', function() {
     nav.classList.remove('navigation-open');
+    document.body.classList.remove('stop-scrolling');
 });
