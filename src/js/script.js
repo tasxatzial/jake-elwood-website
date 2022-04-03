6fe47cb63fddc9a46964e6f6dc35c5cf7be66624
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-04-03 21:05:14 +0300

    fix: tab focus when the menu is open,closed

--- src/js/script.js
+++ src/js/script.js
@@ -1,42 +1,74 @@
 const openNavBtn = document.getElementById('open-nav-btn');
-const closeNavBtn = document.getElementById('close-nav-btn');
 const nav = document.getElementById('nav');
-const focusable = document.querySelectorAll('[tabindex="0"]');
 const navLinks = nav.querySelectorAll('.nav-link');
+const closeNavBtn = nav.querySelector('.close-nav-btn');
+const tabFocusable = document.querySelectorAll('[tabindex="0"]');
+const mqList = window.matchMedia("(min-width: 48rem)");
 
+mqList.addEventListener('change', closeNavMqList);
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
 
-function setFocusable(val) {
-    for (let i = 0; i < focusable.length; i++) {
-        focusable[i].setAttribute('tabindex', val);
-    }
-}
-
-function setMenuVisible() {
-    closeNavBtn.classList.remove('invisible');
+function setLinksVisible() {
     for (let i = 0; i < navLinks.length; i++) {
         navLinks[i].classList.remove('invisible');
+        navLinks[i].classList.add('visible');
     }
 }
 
-function setMenuInvisible() {
-    closeNavBtn.classList.add('invisible');
+function setNavLinksInvisible() {
     for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].classList.remove('visible');
         navLinks[i].classList.add('invisible');
     }
+}
+
+function setTabFocusable(val) {
+    for (let i = 0; i < tabFocusable.length; i++) {
+        tabFocusable[i].setAttribute('tabindex', val);
+    }
+}
+
+function unsetTabFocusable() {
+    for (let i = 0; i < tabFocusable.length; i++) {
+        tabFocusable[i].setAttribute('tabindex', '-1');
+    }
+}
+
+function setCloseNavBtnVisible() {
+    closeNavBtn.classList.remove('invisible');
+    closeNavBtn.classList.add('visible');
+}
+
+function setCloseNavBtnInvisible() {
+    closeNavBtn.classList.remove('visible');
+    closeNavBtn.classList.add('invisible');
 }
 
 function openNav() {
     nav.classList.add('navigation-open');
     document.body.classList.add('stop-scrolling');
-    setMenuVisible();
-    setFocusable('-1');
+    setCloseNavBtnVisible();
+    setLinksVisible();
+    unsetTabFocusable();
 }
 
 function closeNav() {
     nav.classList.remove('navigation-open');
     document.body.classList.remove('stop-scrolling');
-    setMenuInvisible();
-    setFocusable('0');
+    setCloseNavBtnInvisible();
+    setNavLinksInvisible();
+    setTabFocusable();
+}
+
+function closeNavMqList() {
+    if (mqList.matches) {
+        nav.classList.remove('navigation-open');
+        document.body.classList.remove('stop-scrolling');
+        setCloseNavBtnInvisible();
+        setLinksVisible();
+        setTabFocusable();
+    } else {
+        setNavLinksInvisible();
+    }
 }
