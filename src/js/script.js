08a9df19c04bc9b6c7fb7783e3ed10622ef424ec
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-04-03 14:20:17 +0300

    make sure only the nav menu elements are focusable when the menu is open

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,32 @@
 const openNavBtn = document.getElementById('open-nav-btn');
 const closeNavBtn = document.getElementById('close-nav-btn');
 const nav = document.getElementById('nav');
+const focusable = document.querySelectorAll('[tabindex="0"]');
+const navLinks = nav.querySelectorAll('.nav-link');
+
+function setFocusable(val) {
+    for (let i = 0; i < focusable.length; i++) {
+        focusable[i].setAttribute('tabindex', val);
+    }
+}
+
+function setNavMenuVisibility(visibility) {
+    closeNavBtn.style.visibility = visibility;
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].style.visibility = visibility;
+    }
+}
 
 openNavBtn.addEventListener('click', function() {
     nav.classList.add('navigation-open');
     document.body.classList.add('stop-scrolling');
+    setNavMenuVisibility('visible');
+    setFocusable('-1');
 });
 
 closeNavBtn.addEventListener('click', function() {
     nav.classList.remove('navigation-open');
     document.body.classList.remove('stop-scrolling');
+    setNavMenuVisibility('hidden');
+    setFocusable('0');
 });
