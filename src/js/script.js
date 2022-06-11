4e7578c42273bde0f11b4ee3250705392cd77b01
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-06-12 02:34:38 +0300

    trap focus inside nav when it's open

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const openNavBtn = document.getElementById('open-nav-btn');
+const nav = document.querySelector('nav');
 const closeNavBtn = nav.querySelector('.close-nav-btn');
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = nav.querySelectorAll('.nav-link');
 const mqList = window.matchMedia("(min-width: 48rem)");
 
 openNavBtn.addEventListener('click', openNav);
@@ -8,6 +9,22 @@ closeNavBtn.addEventListener('click', closeNav);
 mqList.addEventListener('change', function() {
     if (mqList.matches && document.body.classList.contains('js-nav-open')) {
         closeNav();
+    }
+});
+
+nav.addEventListener('keydown', function(e) {
+    if (e.key === 'Tab' || e.keyCode === 9) {
+        if (e.shiftKey) {
+            if (document.activeElement === closeNavBtn) {
+                navLinks[navLinks.length - 1].focus();
+                e.preventDefault();
+            }
+        } else {
+            if (document.activeElement === navLinks[navLinks.length - 1]) {
+                closeNavBtn.focus();
+                e.preventDefault();
+            }
+        }
     }
 });
 
